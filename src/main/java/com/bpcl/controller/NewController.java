9970828da42c55677a428083d5e89d1ff1914198
kalyanreddyplr <kalyanreddyplr@gmail.com>
2025-10-29 11:01:26 +0530

    this is commit 2

---
+++ src/main/java/com/bpcl/controller/NewController.java
@@ -0,0 +1,12 @@
+package com.bpcl.controller;
+
+public class NewController {
+
+	
+	public void test2() {
+
+		System.out.println("This is NewController");
+
+	}
+
+}
--- src/main/java/com/bpcl/controller/SubbuController.java
+++ src/main/java/com/bpcl/controller/SubbuController.java
@@ -1,12 +1,11 @@
 package com.bpcl.controller;
 
 public class SubbuController {
-	
-	public void test2()
-	{
-		
+
+	public void test2() {
+
 		System.out.println("This is subbu");
-		
+
 	}
 
 }
